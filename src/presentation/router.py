0c12eed8c165c3c5fbd0fb3458b934d2ca768c0a
Amirul Zafrin <amirulzafrin98@gmail.com>
2025-10-10 14:54:58 +0800

    exclude html from api docs

--- src/presentation/router.py
+++ src/presentation/router.py
@@ -4,5 +4,5 @@ from src.presentation.api.router import router as API_ROUTER
 from src.presentation.html.router import router as HTML_ROUTER
 
 router = APIRouter(prefix="")
-router.include_router(HTML_ROUTER)
+router.include_router(HTML_ROUTER, include_in_schema=False)
 router.include_router(API_ROUTER)
